08dd558b593256cbea65e8dc9da3de9092c17b12
margemma <margaux.basart@yahoo.com>
2025-02-05 10:52:35 -0700

    Add del/psi plot
    I added a plot of Psis and Dels. It shows that we have the right plot shape, but wrong values for individual thicknesses. This also means I changed beta and RS/RP to functions

--- ellipsometry.py
+++ ellipsometry.py
@@ -3,9 +3,9 @@
 
 import math
 import cmath
+import matplotlib.pyplot as plt
 
 wavelength = 633 # nm
-d = 100 # film thickness, in nm
 N1 = 1 # refractive index of air
 phi1 = math.radians(70) # radians
 N2 = 1.46 # refractive index of SiO2
@@ -18,12 +18,33 @@ rs12 = (N1 * math.cos(phi1) - N2 * math.cos(phi2))/(N1 * math.cos(phi1) + N2 * m
 rp23 = (N3 * math.cos(phi2) - N2 * cmath.cos(phi3))/(N3 * math.cos(phi2) + N2 * cmath.cos(phi3))
 rs23 = (N2 * math.cos(phi2) - N3 * cmath.cos(phi3))/(N2 * math.cos(phi2) + N3 * cmath.cos(phi3))
 
-beta = (2 * math.pi * N2 * d * math.cos(phi2)) / wavelength
+def beta_func(d):
+    beta = (2 * math.pi * N2 * d * math.cos(phi2)) / wavelength
+    return beta
 
-RP = (rp12 + rp23 * cmath.exp(-2j * beta))/(1 + rp12 * rp23 * cmath.exp(-2j * beta))
-RS = (rs12 + rs23 * cmath.exp(-2j * beta))/(1 + rs12 * rs23 * cmath.exp(-2j * beta))
+def RP_RS(beta):
+    RP = (rp12 + rp23 * cmath.exp(-2j * beta))/(1 + rp12 * rp23 * cmath.exp(-2j * beta))
+    RS = (rs12 + rs23 * cmath.exp(-2j * beta))/(1 + rs12 * rs23 * cmath.exp(-2j * beta))
+    return RP, RS
 
-Psi = math.degrees(cmath.atan(abs(RP)/abs(RS)).real)
-print(Psi)
-Del = math.degrees((cmath.log((RP/RS)/cmath.tan(math.radians(Psi)))/(1j)).real)
-print(Del)
+dels = []
+psis = []
+d = 100
+for i in range(2):
+    beta = beta_func(d)
+    RP, RS = RP_RS(beta)
+    Psi = math.degrees(cmath.atan(abs(RP)/abs(RS)).real)
+    Del = math.degrees((cmath.log((RP/RS)/cmath.tan(math.radians(Psi)))/(1j)).real)
+    if Del < 0:
+        Del += 360
+    psis.append(Psi)
+    dels.append(Del)
+    d += 100
+
+plt.scatter(psis, dels)
+plt.xlim(0, 90)
+plt.ylim(0, 360)
+plt.xlabel('Psi (degrees)')
+plt.ylabel('Delta (degrees)')
+plt.title('Psi vs Delta')
+plt.show()
