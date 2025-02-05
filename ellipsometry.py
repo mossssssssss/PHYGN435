c211bab999aff24a51b9ac8931fd56eff58dd8b3
margemma <margaux.basart@yahoo.com>
2025-02-05 11:10:49 -0700

    Final draft
    Updated the plot so that it looks nice and exactly like the textbook, prints out value of interest at d = 100 nm.

--- ellipsometry.py
+++ ellipsometry.py
@@ -1,11 +1,11 @@
 
-# Margaux's version
 
 import math
 import cmath
 import matplotlib.pyplot as plt
 
 wavelength = 633 # nm
+d = 100 # nm
 N1 = 1 # refractive index of air
 phi1 = math.radians(70) # radians
 N2 = 1.46 # refractive index of SiO2
@@ -19,7 +19,7 @@ rp23 = (N3 * math.cos(phi2) - N2 * cmath.cos(phi3))/(N3 * math.cos(phi2) + N2 * 
 rs23 = (N2 * math.cos(phi2) - N3 * cmath.cos(phi3))/(N2 * math.cos(phi2) + N3 * cmath.cos(phi3))
 
 def beta_func(d):
-    beta = (2 * math.pi * N2 * d * math.cos(phi2)) / wavelength
+    beta = 2 * math.pi * (d / wavelength) * N2 * math.cos(phi2)
     return beta
 
 def RP_RS(beta):
@@ -27,21 +27,37 @@ def RP_RS(beta):
     RS = (rs12 + rs23 * cmath.exp(-2j * beta))/(1 + rs12 * rs23 * cmath.exp(-2j * beta))
     return RP, RS
 
-dels = []
-psis = []
-d = 100
-for i in range(2):
-    beta = beta_func(d)
-    RP, RS = RP_RS(beta)
+def Psi_Del(RP, RS):
     Psi = math.degrees(cmath.atan(abs(RP)/abs(RS)).real)
     Del = math.degrees((cmath.log((RP/RS)/cmath.tan(math.radians(Psi)))/(1j)).real)
     if Del < 0:
         Del += 360
-    psis.append(Psi)
-    dels.append(Del)
-    d += 100
+    return Psi, Del
 
-plt.scatter(psis, dels)
+beta = beta_func(d)
+RP, RS = RP_RS(beta)
+Psi, Del = Psi_Del(RP, RS)
+print("Psi: ", Psi, "\nDel: ", Del)
+
+# This next part is just for funsies, to show that our code recreates Figure 3-6
+del_points = []
+psi_points = []
+all_dels = []
+all_psis = []
+d = 0
+for i in range(280):
+    beta = beta_func(d)
+    RP, RS = RP_RS(beta)
+    Psi, Del = Psi_Del(RP, RS)
+    all_psis.append(Psi)
+    all_dels.append(Del)
+    if d % 10 == 0:
+        del_points.append(Del)
+        psi_points.append(Psi)
+    d += 1
+
+plt.scatter(psi_points, del_points)
+plt.plot(all_psis, all_dels)
 plt.xlim(0, 90)
 plt.ylim(0, 360)
 plt.xlabel('Psi (degrees)')
