c45c3346c4fa274b2a2763fa937259a469b28973
margemma <margaux.basart@yahoo.com>
2025-02-05 11:14:33 -0700

    Final final draft
    Added comments for clarity, it's actually completed now!

--- ellipsometry.py
+++ ellipsometry.py
@@ -1,9 +1,9 @@
 
-
 import math
 import cmath
 import matplotlib.pyplot as plt
 
+# System information, filled in with Snell's Law
 wavelength = 633 # nm
 d = 100 # nm
 N1 = 1 # refractive index of air
@@ -13,11 +13,13 @@ phi2 = math.asin(N1 * math.sin(phi1) / N2) # radians
 N3 = 3.872 - 0.037j # refractive index of Si
 phi3 = cmath.asin(N2 * math.sin(phi2) / N3) # radians
 
+# Fresnel equations
 rp12 = (N2 * math.cos(phi1) - N1 * math.cos(phi2))/(N2 * math.cos(phi1) + N1 * math.cos(phi2))
 rs12 = (N1 * math.cos(phi1) - N2 * math.cos(phi2))/(N1 * math.cos(phi1) + N2 * math.cos(phi2))
 rp23 = (N3 * math.cos(phi2) - N2 * cmath.cos(phi3))/(N3 * math.cos(phi2) + N2 * cmath.cos(phi3))
 rs23 = (N2 * math.cos(phi2) - N3 * cmath.cos(phi3))/(N2 * math.cos(phi2) + N3 * cmath.cos(phi3))
 
+# Functions to calculate beta, RP, RS, Psi, and Del from equations 20, 19, 23, and 25
 def beta_func(d):
     beta = 2 * math.pi * (d / wavelength) * N2 * math.cos(phi2)
     return beta
@@ -34,12 +36,14 @@ def Psi_Del(RP, RS):
         Del += 360
     return Psi, Del
 
+# Call the functions for d = 100 nm
 beta = beta_func(d)
 RP, RS = RP_RS(beta)
 Psi, Del = Psi_Del(RP, RS)
 print("Psi: ", Psi, "\nDel: ", Del)
 
 # This next part is just for funsies, to show that our code recreates Figure 3-6
+# This is the whole reason I even bothered with defining functions above
 del_points = []
 psi_points = []
 all_dels = []
